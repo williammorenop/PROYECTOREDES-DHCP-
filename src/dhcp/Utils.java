986581c12db0b9cd78446b1c5e000b6e118ab24b
William Moreno <williammorenoprieto@gmail.com>
2017-11-18 00:06:22 -0500

    Archivo

--- src/dhcp/Utils.java
+++ src/dhcp/Utils.java
@@ -5,23 +5,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class Utils {
 	
-	public static void leerArchivo(String nombre)
+	public static void leerArchivo(String nombre,List<Red> Redes)
 	{
 		File archivo = new File (".\\Log.txt");
 		FileReader fr = null;
 		try {
 			fr = new FileReader (archivo);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		BufferedReader br = new BufferedReader(fr);
 		try {
-			String linea = br.readLine();
-			System.out.println("---> "+linea);
+			String linea,name,ip,mask,dns,gateway;
+			
+			while((linea = br.readLine())!=null)
+			{
+				System.out.println("---> "+linea);				
+				if(linea.equals("------------"))
+				{
+					name=br.readLine();
+					ip=br.readLine();
+					mask=br.readLine();
+					dns=br.readLine();
+					gateway=br.readLine();
+				}
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
